27213b3197196d472771f971b0e7ac8ef43207f4
Syarif Hidayat <Syarif_H657@mitrais.com>
2019-03-13 10:02:17 +0700

    add userReducer, userAction, mapStateToProps and mapActionToProps

--- src/App.js
+++ src/App.js
@@ -1,28 +1,46 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { updateUser } from "./action/userAction";
+
+import { connect } from "react-redux";
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.onUpdateUser = this.onUpdateUser.bind(this);
+  }
+
+  onUpdateUser(){
+    this.props.onUpdateUser('Goerge Harisson');
+  }
+
   render() {
+    console.log(this.props);
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <header>
         </header>
+        <main>
+          <p>Check inspect element for checking the state</p>
+
+          <div onClick={this.onUpdateUser}>
+            <p>Or Clik Me to Update User !!!</p>
+          </div>
+          { this.props.user }
+        </main>
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => ({
+  products: state.products,
+  user: state.user
+});
+
+const mapActionsToProps = {
+  onUpdateUser: updateUser
+};
+
+export default connect(mapStateToProps,mapActionsToProps)(App);
